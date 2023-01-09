61380402e40bd9db52122b373bd87237d531440c
lamkimphu258 <lamkimphu258@gmail.com>
2023-01-09 16:54:53 +0700

    Feature: Add layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>{{ $title ?? 'Thich Hoc' }}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    @livewireStyles
+</head>
+
+<body>
+    <header>
+        <livewire:navbar/>
+    </header>
+    <main>{{ $slot }}</main>
+    <footer></footer>
+
+    @livewireScripts
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/login', function() {})->name('login');
+Route::get('/signup', function() {})->name('signup');
